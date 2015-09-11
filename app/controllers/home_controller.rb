ff393cfc797e253c2d78026ff7e24ae7125e6a36
Laurie Young <laurie@wildfalcon.com>
2015-09-11 16:27:41 +0100

    Log into Instagram and store the credentials in redis

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,5 +3,10 @@ class HomeController < ApplicationController
   def index
   end
 
-
+  def callback
+    hash = request.env["omniauth.auth"]
+    $redis.set("instagram_token", hash["credentials"]["token"])
+    $redis.set("uid", hash["uid"])
+    redirect_to root_path
+  end
 end
---
+++ config/initializers/omniauth.rb
@@ -0,0 +1,5 @@
+Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :instagram, ENV["INSTAGRAM_ID"],
+                       ENV["INSTAGRAM_SECRET"],
+                       :scope => "basic relationships"                      
+end
